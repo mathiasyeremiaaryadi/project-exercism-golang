535dc42e4ae2dac6d89fd25b4b16f10491e08a06
Mathias Yeremia Aryadi <mathias.yeremia@it.sharingvision.com>
2025-08-20 14:33:55 +0700

    changes

---
+++ 29-two-fer/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// ShareWith should have a comment documenting it.
+func ShareWith(name string) string {
+	// Write some code here to pass the test suite.
+	// Then remove all the stock comments.
+	// They're here to help you get started but they only clutter a finished solution.
+	// If you leave them in, reviewers may protest!
+	if name == "" {
+		return "One for you, one for me."
+	}
+
+	return "One for " + name + ", one for me."
+}
+
+func main() {
+	fmt.Println(ShareWith("you"))
+}
